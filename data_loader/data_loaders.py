d80a83a6e1879e6142770f7147329c4f5af7c814
yong-again <musehead91@gmail.com>
2025-06-10 04:13:45 +0900

    add swin transformer model, augmix, albumentation, label smoothing

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('../')
 from base import BaseDataLoader
 from utils.util import get_parent_path
-from data_loader.transform import train_transform, valid_transform, train_transform_albu, valid_transform_albu
+from data_loader.transform import train_transform, valid_transform, train_transform_albu, valid_transform_albu, AlbumentationsWithAugMix
 import numpy as np
 
 import logging
@@ -81,6 +81,72 @@ class CarImageDataLoader(BaseDataLoader):
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
         
         
+class CarAugImageDataset(Dataset):
+    """
+    Car Image Classification dataset
+    """
+    def __init__(self, data_dir, csv_file, return_onehot=False, transform=None):
+        self.data_dir = data_dir
+        self.csv_file = os.path.join(get_parent_path(), data_dir, 'train_csv', csv_file)
+        self.return_onehot = return_onehot
+        self.transform = transform
+        self.data_frame = pd.read_csv(self.csv_file)
+        self.num_classes = len(self.data_frame['label_index'].unique())
+        
+    def __len__(self):
+        return len(self.data_frame)
+    
+    def __getitem__(self, idx):
+        try:
+            row = self.data_frame.iloc[idx]
+            img_path = os.path.basename(row['image_path'])
+            folder_name = row['folder_name']
+            img_full_path = f"{get_parent_path()}/{self.data_dir}/train/{folder_name}/{img_path}"
+            image = Image.open(img_full_path).convert('RGB')
+            image = np.array(image)
+            
+            if self.transform:
+                transformed = self.transform(image=image)
+                image = transformed['image']
+                
+            
+            label = int(row['label_index'])
+            
+            if self.return_onehot:
+                label = torch.eye(self.num_classes)[label]
+                print(f"One-hot encoded label shape: {label.shape}")
+            else:    
+                label = torch.tensor(label, dtype=torch.long)
+                # print(f"Label shape: {label.shape}")
+                # print(f"Label value: {label.item()}")
+                
+                
+            if label is None:
+                raise ValueError(f"Label is None for idx={idx} in {img_full_path}")
+
+            return image, label
+        
+        except Exception as e:
+            logging.error(f"{img_full_path} | idx={idx} | error={str(e)}")
+            return None
+        
+    def clone_with_transform(self, transform):
+        return CarAugImageDataset(
+            data_dir=self.data_dir,
+            csv_file=self.csv_file,
+            transform=transform,
+        )
+
+class CarAugImageDataLoader(BaseDataLoader):
+    """
+    Car Image Classfication dataset
+    """
+    def __init__(self, data_dir, csv_file, batch_size, shuffle=True, validation_split=0.0, num_workers=2, return_onehot=False):
+        trsfm = train_transform_albu()
+        self.data_dir = data_dir
+        self.dataset = CarAugImageDataset(data_dir=self.data_dir, csv_file=csv_file, return_onehot=return_onehot, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
 class CarTestDataSet(Dataset):
     """
     Car Image Classification dataset for testing
@@ -122,8 +188,8 @@ class CarTestDataLoader(BaseDataLoader):
 if __name__ == '__main__':
     from utils.util import get_parent_path
     parent_path = get_parent_path()
-    data_dir = get_parent_path() / "data"
-    car_loader = CarTestDataLoader(data_dir=data_dir, csv_file='test.csv', batch_size=32)
+    data_dir = get_parent_path() / "data" 
+    car_loader = CarAugImageDataLoader(data_dir=data_dir, csv_file='train_mapped_2.csv', batch_size=32)
     
     for images, labels in car_loader:
         print(images.shape, labels.shape)
